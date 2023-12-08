b9b97d6eae9fb2d91979b50036059012c578e0ff
Dior Miss <uhij90@fssuniverse.com>
2023-12-08 12:38:05 +0900

    Initial commit

---
+++ src/java/Miner.java
@@ -0,0 +1,8 @@
+public class Miner {
+    class Solution {
+        public int solution(int[] picks, String[] minerals) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
